aaa55066cb40830bc224544793126d1a041c8abc
Rob Symes <robsymes@gmail.com>
2012-10-26 20:23:11 +0100

    initial setup

---
+++ FinProj/FinProjMvc/Controllers/ProjectionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinProjMvc.Controllers
+{
+    public class ProjectionController : Controller
+    {
+        //
+        // GET: /Projection/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
